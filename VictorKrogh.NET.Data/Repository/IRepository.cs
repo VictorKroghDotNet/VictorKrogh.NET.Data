3342e96218e1df21a7b9e89307d2859ea784cd0b
KingoBoiii <kingodk@outlook.dk>
2023-04-06 21:16:07 +0200

    Added base repository interface and class

---
+++ VictorKrogh.NET.Data/Repository/IRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace VictorKrogh.NET.Data.Repository;
+
+public interface IRepository
+{
+}
+
+public interface IRepository<TModel> : IRepository where TModel : class
+{
+}
+
+public interface IWriteRepository<TModel> : IRepository<TModel> where TModel : class
+{
+    Task<bool> AddAsync(TModel model);
+    Task<bool> UpdateAsync(TModel model);
+    Task<bool> AddOrUpdateAsync(TModel model);
+    Task<bool> DeleteAsync(TModel model);
+    Task<bool> DeleteAllAsync();
+}
+
+public interface IReadOnlyRepository<TModel> : IRepository<TModel> where TModel : class
+{
+    Task<TModel?> GetFirstOrDefaultAsync();
+    Task<IEnumerable<TModel?>> GetAsync();
+}
+
+public interface IReadOnlyRepository<TModel, TKey> : IReadOnlyRepository<TModel>
+    where TModel : class
+    where TKey : notnull
+{
+    Task<TModel?> GetAsync(TKey key);
+}
+
+public interface IRepository<TModel, TKey> : IReadOnlyRepository<TModel, TKey>, IWriteRepository<TModel>
+    where TModel : class
+    where TKey : notnull
+{
+}
---
+++ VictorKrogh.NET.Data/Repository/RepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using VictorKrogh.NET.Data.Provider;
+
+namespace VictorKrogh.NET.Data.Repository;
+
+public abstract class RepositoryBase<TProvider> where TProvider : IProvider
+{
+    public RepositoryBase(TProvider provider)
+    {
+        Provider = provider;
+    }
+
+    protected TProvider Provider { get; }
+}
