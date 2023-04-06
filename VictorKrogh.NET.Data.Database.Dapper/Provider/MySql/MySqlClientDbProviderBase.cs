0329918e95b12dfb1e1174bfd5000defa531cf67
KingoBoiii <kingodk@outlook.dk>
2023-04-06 22:04:41 +0200

    Added MySql ProviderBase class implementation. Added MySql Provider Factory. Added MySql Provider Settings

---
+++ VictorKrogh.NET.Data.Database.Dapper/Provider/MySql/MySqlClientDbProviderBase.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using VictorKrogh.NET.Data.Database.Provider;
+
+namespace VictorKrogh.NET.Data.Database.Dapper.Provider.MySql;
+
+public interface IMySqlClientDbProvider : IDbProvider
+{
+}
+
+public abstract class MySqlClientDbProviderBase : DapperDbProviderBase, IMySqlClientDbProvider
+{
+    public MySqlClientDbProviderBase(IsolationLevel isolationLevel, MySqlClientDbProviderSettings mySqlClientDbProviderSettings) 
+        : base(isolationLevel)
+    {
+        MySqlClientDbProviderSettings = mySqlClientDbProviderSettings ?? throw new ArgumentNullException(nameof(mySqlClientDbProviderSettings));
+    }
+
+    private MySqlClientDbProviderSettings? MySqlClientDbProviderSettings { get; }
+
+    protected override IDbConnection CreateConnection()
+    {
+        return new MySqlConnection(MySqlClientDbProviderSettings?.ConnectionString);
+    }
+}
---
+++ VictorKrogh.NET.Data.Database.Dapper/Provider/MySql/MySqlClientDbProviderFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Reflection;
+using VictorKrogh.NET.Data.Provider;
+
+namespace VictorKrogh.NET.Data.Database.Dapper.Provider.MySql;
+
+public sealed class MySqlClientDbProviderFactory<TProvider> : IProviderFactory<TProvider>
+    where TProvider : IMySqlClientDbProvider
+{
+    public MySqlClientDbProviderFactory(MySqlClientDbProviderSettings mySqlClientDbProviderSettings)
+    {
+        MySqlClientDbProviderSettings = mySqlClientDbProviderSettings;
+    }
+
+    private MySqlClientDbProviderSettings MySqlClientDbProviderSettings { get; }
+
+    public TProvider CreateProvider(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+    {
+        var type = typeof(TProvider);
+
+        var ctor = type.GetConstructor(BindingFlags.Instance | BindingFlags.Public, null, new[] { typeof(IsolationLevel), typeof(MySqlClientDbProviderSettings) }, null);
+        if (ctor == null)
+        {
+            throw new NotImplementedException($"The type {type.FullName} does not have a constructor with the signature {typeof(IsolationLevel).FullName} and {typeof(MySqlClientDbProviderSettings).FullName}.");
+        }
+
+        var instance = (TProvider)ctor.Invoke(new object[] { isolationLevel, MySqlClientDbProviderSettings });
+
+        return instance;
+    }
+}
