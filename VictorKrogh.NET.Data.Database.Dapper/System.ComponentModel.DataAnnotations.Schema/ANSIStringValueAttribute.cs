520cccd00a98fbd9ab549e0b88ef88d167810065
KingoBoiii <kingodk@outlook.dk>
2023-04-06 21:51:03 +0200

    Added DapperDbProviderBase and Dapper extensions

---
+++ VictorKrogh.NET.Data.Database.Dapper/System.ComponentModel.DataAnnotations.Schema/ANSIStringValueAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace System.ComponentModel.DataAnnotations.Schema;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+public class ANSIStringValueAttribute : Attribute
+{
+    public ANSIStringValueAttribute()
+    {
+        FixedLength = null;
+    }
+
+    public ANSIStringValueAttribute(int fixedLength)
+    {
+        FixedLength = fixedLength;
+    }
+
+    public int? FixedLength { get; }
+}
---
+++ VictorKrogh.NET.Data.Database.Dapper/System.ComponentModel.DataAnnotations.Schema/PropertyBeforeSaveBehaviorAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace System.ComponentModel.DataAnnotations.Schema;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+public class PropertyBeforeSaveBehaviorAttribute : Attribute
+{
+    public PropertyBeforeSaveBehaviorAttribute(bool ignore = false)
+    {
+        Ignore = ignore;
+    }
+
+    public bool Ignore { get; }
+}
