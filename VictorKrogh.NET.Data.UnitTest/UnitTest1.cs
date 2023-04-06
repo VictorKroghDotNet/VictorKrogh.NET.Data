bb5bd8eaf6499c695ed8530232e28a977b58e549
KingoBoiii <kingodk@outlook.dk>
2023-04-06 21:51:17 +0200

    Added Unit Test project

---
+++ VictorKrogh.NET.Data.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace VictorKrogh.NET.Data.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
