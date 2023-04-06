a18ae625c113c36d56e5f66fda644633c98ce7ec
KingoBoiii <kingodk@outlook.dk>
2023-04-06 21:24:59 +0200

    Added Unit of Work implmenentation

---
+++ VictorKrogh.NET.Data/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+using VictorKrogh.NET.Data.Provider;
+using VictorKrogh.NET.Data.Repository;
+using VictorKrogh.NET.Disposable;
+
+namespace VictorKrogh.NET.Data;
+
+internal sealed class UnitOfWork : DisposableObject, IUnitOfWork
+{
+    public UnitOfWork(IServiceProvider serviceProvider, IsolationLevel isolationLevel)
+    {
+        ServiceProvider = serviceProvider;
+        IsolationLevel = isolationLevel;
+
+        Providers = new List<IProvider>();
+    }
+
+    private IServiceProvider ServiceProvider { get; }
+    public IsolationLevel IsolationLevel { get; }
+    public bool IsCompleted { get; private set; }
+    private IList<IProvider> Providers { get; }
+
+    public void Complete()
+    {
+        foreach (var provider in Providers)
+        {
+            provider.Complete();
+        }
+
+        IsCompleted = true;
+    }
+
+    public TRepository GetRepository<TRepository>() where TRepository : IRepository
+    {
+        return ServiceProvider.GetRequiredService<TRepository>();
+    }
+
+    public TProvider CreateProvider<TProvider>(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted) where TProvider : IProvider
+    {
+        var providerFactory = ServiceProvider.GetRequiredService<IProviderFactory<TProvider>>();
+
+        var provider = providerFactory.CreateProvider(isolationLevel);
+
+        Providers.Add(provider);
+
+        return provider;
+    }
+
+    protected override void DisposeManagedState()
+    {
+        Providers.Clear();
+    }
+}
---
+++ VictorKrogh.NET.Data/UnitOfWorkFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+
+namespace VictorKrogh.NET.Data;
+
+public interface IUnitOfWorkFactory
+{
+    IUnitOfWork Create(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted);
+}
+
+internal sealed class UnitOfWorkFactory : IUnitOfWorkFactory
+{
+    public UnitOfWorkFactory(IServiceProvider serviceProvider)
+    {
+        ServiceProvider = serviceProvider;
+    }
+
+    private IServiceProvider ServiceProvider { get; }
+
+    public IUnitOfWork Create(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+    {
+        return new UnitOfWork(ServiceProvider, isolationLevel);
+    }
+}
