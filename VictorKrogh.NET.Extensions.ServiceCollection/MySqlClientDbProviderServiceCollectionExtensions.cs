52a9d871a0afd8a7e2606a742cd9b85f282e9ec5
KingoBoiii <kingodk@outlook.dk>
2023-04-07 10:31:20 +0200

    Added ServiceCollection extensions for registering Unit of Work, Repository and MySqlClientDbProvider

---
+++ VictorKrogh.NET.Extensions.ServiceCollection/MySqlClientDbProviderServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VictorKrogh.NET.Data.Database.Dapper.Provider.MySql;
+using VictorKrogh.NET.Data.Provider;
+
+namespace VictorKrogh.NET.Extensions.DependencyInjection;
+
+public static class MySqlClientDbProviderServiceCollectionExtensions
+{
+    public static IServiceCollection AddMySqlClientProviderFactory<TProvider>(this IServiceCollection services, MySqlClientDbProviderSettings mySqlClientDbProviderSettings)
+        where TProvider : class, IMySqlClientDbProvider
+    {
+        services.AddScoped<IProviderFactory<TProvider>, MySqlClientDbProviderFactory<TProvider>>(serviceProvider =>
+        {
+            return new MySqlClientDbProviderFactory<TProvider>(mySqlClientDbProviderSettings);
+        });
+
+        return services;
+    }
+}
---
+++ VictorKrogh.NET.Extensions.ServiceCollection/UnitOfWorkServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VictorKrogh.NET.Data;
+
+namespace VictorKrogh.NET.Extensions.DependencyInjection;
+
+public static class UnitOfWorkServiceCollectionExtensions
+{
+    public static IServiceCollection AddUnitOfWork(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWorkFactory, UnitOfWorkFactory>();
+
+        services.AddScoped<IUnitOfWork>(serviceProvider =>
+        {
+            var unitOfWorkFactory = serviceProvider.GetRequiredService<IUnitOfWorkFactory>();
+
+            return unitOfWorkFactory.Create();
+        });
+
+        return services;
+    }
+}
