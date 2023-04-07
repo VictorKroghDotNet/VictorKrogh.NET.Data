faa616ddaddbef2252a180dcc7ebd592c34f402f
KingoBoiii <kingodk@outlook.dk>
2023-04-07 11:20:48 +0200

    Added ServiceCollectionTests.

---
+++ VictorKrogh.NET.Data.UnitTest/Mock/MockVKProjectRepository.cs
@@ -0,0 +1,17 @@
+﻿using VictorKrogh.NET.Data.Database.Provider;
+using VictorKrogh.NET.Data.Database.Repository;
+using VictorKrogh.NET.Data.Repository;
+
+namespace VictorKrogh.NET.Data.UnitTest.Mock;
+
+internal interface IMockVKProjectRepository : IRepository<MockVKProject, int>
+{
+}
+
+internal class MockVKProjectRepository : DbRepositoryBase<MockVKProject, int>, IMockVKProjectRepository
+{
+    public MockVKProjectRepository(IMockMySqlClientDbClientProvider provider) 
+        : base(provider)
+    {
+    }
+}
