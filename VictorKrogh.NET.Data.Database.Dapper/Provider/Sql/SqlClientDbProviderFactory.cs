b2aeb44b18dc7fa03d01aa91e5d49287d7234b15
KingoBoiii <kingodk@outlook.dk>
2023-04-22 20:00:00 +0200

    Added MSSQL ProviderBase, ProviderFactory and ProviderSettings.

---
+++ VictorKrogh.NET.Data.Database.Dapper/Provider/Sql/SqlClientDbProviderFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Reflection;
+using VictorKrogh.NET.Data.Provider;
+
+namespace VictorKrogh.NET.Data.Database.Dapper.Provider.Sql;
+
+public sealed class SqlClientDbProviderFactory<TProvider> : IProviderFactory<TProvider>
+    where TProvider : ISqlClientDbProvider
+{
+    public SqlClientDbProviderFactory(SqlClientDbProviderSettings sqlClientDbProviderSettings)
+    {
+        SqlClientDbProviderSettings = sqlClientDbProviderSettings;
+    }
+
+    private SqlClientDbProviderSettings SqlClientDbProviderSettings { get; }
+
+    public TProvider CreateProvider(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+    {
+        var type = typeof(TProvider);
+
+        var ctor = type.GetConstructor(BindingFlags.Instance | BindingFlags.Public, null, new[] { typeof(IsolationLevel), typeof(SqlClientDbProviderSettings) }, null);
+        if (ctor == null)
+        {
+            throw new NotImplementedException($"The type {type.FullName} does not have a constructor with the signature {typeof(IsolationLevel).FullName} and {typeof(SqlClientDbProviderSettings).FullName}.");
+        }
+
+        var instance = (TProvider)ctor.Invoke(new object[] { isolationLevel, SqlClientDbProviderSettings });
+
+        return instance;
+    }
+}
---
+++ VictorKrogh.NET.Data.Database.Dapper/Provider/Sql/SqlClientDbProviderSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlClient;
+using VictorKrogh.NET.Data.Database.Provider;
+
+namespace VictorKrogh.NET.Data.Database.Dapper.Provider.Sql;
+
+public sealed class SqlClientDbProviderSettings : IDbProviderSettings
+{
+    public string? ConnectionString { get; set; }
+    public SqlCredential? SqlCredential { get; set; }
+}
